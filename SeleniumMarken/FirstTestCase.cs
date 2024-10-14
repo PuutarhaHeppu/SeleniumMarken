41a8844d00b502af876e3bd8d0b2728f6df56866
Opilane TTHK <opilane@tthk.ee>
2024-10-14 10:52:27 +0300

    essa ja viimane commit

---
+++ SeleniumMarken/FirstTestCase.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumMarken
+{
+    public class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "https://localhost:7156/";
+        }
+    }
+}
