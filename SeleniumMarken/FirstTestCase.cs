d3633c04c7f1b777b192d1a0773a2c83216df2d0
PuutarhaHeppu <127192974+PuutarhaHeppu@users.noreply.github.com>
2024-10-30 13:20:57 +0200

    Update FirstTestCase.cs

--- SeleniumMarken/FirstTestCase.cs
+++ SeleniumMarken/FirstTestCase.cs
@@ -1,5 +1,6 @@
-﻿using OpenQA.Selenium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
 
 namespace SeleniumMarken
 {
@@ -7,13 +8,56 @@ namespace SeleniumMarken
     {
         static void Main(string[] args)
         {
+            //TestHidingButton();
+            //TestClickTwice();
+            TestHiddenButtons();
+        }
+
+        private static void TestHidingButton()
+        {
             string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
 
             FirefoxOptions options = new FirefoxOptions();
             options.BrowserExecutableLocation = binaryLocation;
 
             IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
-            driver.Url = "https://localhost:7156/";
+            driver.Url = "http://www.uitestingplayground.com/scrollbars";
+
+            IWebElement element = driver.FindElement(By.Id("hidingButton"));
+            element.Click();
+        }
+
+        private static void TestClickTwice()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/mouseover";
+
+            Actions actions = new Actions(driver);
+            IWebElement element = driver.FindElement(By.ClassName("text-primary"));
+            actions.DoubleClick(element).Perform();
+
+        }
+
+        private static void TestHiddenButtons()
+        {
+            string binaryLocation = "C:\\Users\\opilane\\source\\repos\\FFP\\FirefoxPortable\\App\\Firefox64\\Firefox.exe";
+
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = binaryLocation;
+
+            IWebDriver driver = new FirefoxDriver("C:\\Users\\opilane\\source\\repos\\SeleniumMarken\\SeleniumMarken\\drivers", options);
+            driver.Url = "http://www.uitestingplayground.com/visibility";
+
+            IWebElement element = driver.FindElement(By.Id("hideButton"));
+            element.Click();
+            IWebElement element2 = driver.FindElement(By.Id("removedButton"));
+            element.Click();
+
         }
     }
 }
